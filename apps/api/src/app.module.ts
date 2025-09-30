0b6d9bc706ebf0e1b206d2184d0672b199df7fce
Maliq <maliqadewale@gmail.com>
2025-09-30 11:02:13 -0400

    added instructor and admin modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -8,9 +8,12 @@ import { UsersModule } from './users/users.module';
 import { CoursesModule } from './courses/courses.module';
 import { SectionsModule } from './sections/sections.modules';
 import { StudentsModule } from './students/students.modules';
+import { InstructorsModule } from './instructors/instructors.module';
+import { AdminsModule } from './admins/admins.module';
 
 @Module({
-  imports: [LinksModule, UsersModule, CoursesModule, SectionsModule, StudentsModule],
+  imports: [LinksModule, UsersModule, CoursesModule, SectionsModule, 
+    StudentsModule, InstructorsModule, AdminsModule],
   controllers: [AppController],
   providers: [AppService],
 })
