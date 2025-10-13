a114685766c95b00cfee31fbb68abe6e3547b80f
Maliq <maliqadewale@gmail.com>
2025-10-12 22:47:24 -0400

    changed to hardcoded

--- apps/web-start/src/integrations/fetcher.ts
+++ apps/web-start/src/integrations/fetcher.ts
@@ -2,10 +2,8 @@
 export function backendFetcher<T>(endpoint: string): () => Promise<T> {
   return () => {
     const baseUrl = import.meta.env.DEV 
-      // ? 'http://localhost:3000'  // Development
-      // : 'https://individual-webapp.onrender.com'
-       ?  import.meta.env.VITE_BACKEND_URL_LocalHost
-        : import.meta.env.VITE_BACKEND_URL; // Production
+      ? 'http://localhost:3000'  // Development
+      : 'https://individual-webapp.onrender.com'; // Production
     
     const normalizedEndpoint = endpoint.startsWith('/') ? endpoint : `/${endpoint}`;
     
